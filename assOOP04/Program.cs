db8d091a3e174c1aee610c1c2a6deaa81d16d1e0
Hazem walid <walidf106@gmail.com>
2026-02-28 17:17:50 +0200

    Q3_Part1

--- assOOP04/Program.cs
+++ assOOP04/Program.cs
@@ -15,6 +15,12 @@
             //overloading ->  look at compile time 
             //overridding -> look at run time 
             #endregion
+            #region Q3_Part1
+            //What keywords are used for Method Overriding? What does each one mean ?
+            // ketwords -> in parent class put on method virtual and child class put override
+            //virtual -> can be redefined in a child class
+            // override -> this is new implementation of this method 
+            #endregion
         }
     }
 }
