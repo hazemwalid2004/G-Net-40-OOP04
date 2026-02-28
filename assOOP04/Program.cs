f384074488ad4b966c3aca8a6ec07c617c0d3c7c
Hazem walid <walidf106@gmail.com>
2026-02-28 17:10:25 +0200

    Q1_Part1

--- assOOP04/Program.cs
+++ assOOP04/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            #region Q1_Part1
+            //What is the difference between static binding and dynamic binding? When does each one happen?
+            //static -> Look at the variable type when compile time 
+            //Dynamic -> Look at the object type when run time
+            #endregion
         }
     }
 }
